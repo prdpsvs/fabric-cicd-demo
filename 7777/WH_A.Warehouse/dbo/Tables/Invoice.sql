46ef6e84ad59ef1c769529ea0ae1939b93b79cad
Pradeep Srikakolapu <prdpsvs@gmail.com>
2026-07-18 16:47:19 -0700

    Update Invoice.sql

--- 7777/WH_A.Warehouse/dbo/Tables/Invoice.sql
+++ 7777/WH_A.Warehouse/dbo/Tables/Invoice.sql
@@ -6,3 +6,6 @@ CREATE TABLE [dbo].[Invoice]
     [Amount]      DECIMAL(18,2)   NOT NULL,
     [Currency]    VARCHAR(3)      NULL
 );
+
+ALTER TABLE [dbo].[Invoice]
+    ADD CONSTRAINT [PK_Invoice] PRIMARY KEY NONCLUSTERED ([InvoiceId]) NOT ENFORCED;
