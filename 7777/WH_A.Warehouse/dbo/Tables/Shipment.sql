322a206f528927bae672bf9dbd71aa7c4b5d7d4c
Pradeep Srikakolapu <prdpsvs@gmail.com>
2026-07-18 16:44:15 -0700

    Create Shipment.sql

---
+++ 7777/WH_A.Warehouse/dbo/Tables/Shipment.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dbo.Shipment
+(
+    ShipmentId  INT          NOT NULL,
+    OrderId     INT          NOT NULL,
+    ShippedOn   DATE         NOT NULL,
+    Carrier     VARCHAR(50)  NOT NULL
+);
