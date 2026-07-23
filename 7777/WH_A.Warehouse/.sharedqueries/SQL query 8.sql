4609b9f0adfaaeb5415730b4b6d4dfb403e6ef6e
Pradeep Srikakolapu <prdpsvs@gmail.com>
2026-07-23 13:20:38 -0700

    Committing 1 item from workspace 3a9f8bf6-ff56-4ffe-b632-7a46ec1df2af

---
+++ 7777/WH_A.Warehouse/.sharedqueries/SQL query 8.sql
@@ -0,0 +1,19 @@
+
+
+-- Auto-fix: OBJECT_ID returns NULL if the object does not exist.
+-- Original code created the table only when OBJECT_ID(...) IS NOT NULL (i.e., when it already existed),
+-- which was logically inverted and caused "Invalid column name 'dbo.Deployment'" when referenced later.
+-- Correct logic: create table when it does NOT exist.
+IF OBJECT_ID('dbo.Deployment') IS NULL
+BEGIN
+    CREATE TABLE dbo.Deployment (id INT NULL); -- Auto-fix: Created table in dbo schema with column [id].
+END
+
+IF OBJECT_ID('dbo.Deployment') IS NOT NULL
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM dbo.Deployment) -- Auto-fix: Now valid because table definitely exists.
+        INSERT INTO dbo.Deployment (id)
+        VALUES (1);
+END
+
+ 
---
+++ 7777/WH_A.Warehouse/dbo/Tables/table1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[table1] (
+    [col1] INT NOT NULL
+);
+
+
+GO
